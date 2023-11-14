c7020d9d99dcb69e271c7ebf60c5f73db63d9c67
Vladimir <summeroff@gmail.com>
2023-11-14 15:53:06 -0800

    Update s3upload.ps1

--- s3upload.ps1
+++ s3upload.ps1
@@ -4,39 +4,26 @@ param(
        [string] $symStoreFolder
 )
 
-# Function for verbose logging
-function Write-VerboseLog {
-    param (
-        [string] $message
-    )
-    if ($Env:VERBOSE_SYMB_UPLOAD -eq "true") {
-        Write-Host "VERBOSE: $message"
-    }
-}
-
 # Setting local environment variables
-Write-VerboseLog "Setting AWS environment variables..."
+Write-Host "S3Upload: Setting AWS environment variables..."
 $Env:AWS_ACCESS_KEY_ID = $Env:AWS_SYMB_ACCESS_KEY_ID
 $Env:AWS_SECRET_ACCESS_KEY = $Env:AWS_SYMB_SECRET_ACCESS_KEY
 $Env:AWS_DEFAULT_REGION = "us-east-2"
-Write-VerboseLog "AWS environment variables set."
+Write-Host "S3Upload: AWS environment variables set."
 
-# AWS S3 Copy
-Write-VerboseLog "Starting AWS S3 copy..."
+# AWS S3 Copy with debug option
+Write-Host "S3Upload: Starting AWS S3 copy..."
 try {
-    $awsCommand = "aws s3 cp $symStoreFolder s3://slobs-symbol.streamlabs.com/symbols --recursive --acl public-read"
-    if ($Env:VERBOSE_SYMB_UPLOAD -eq "true") {
-        $awsCommand += " --debug"
-        Write-VerboseLog "Running AWS command with debug mode enabled."
-    }
+    $awsCommand = "aws s3 cp $symStoreFolder s3://slobs-symbol.streamlabs.com/symbols --recursive --acl public-read --debug"
+    Write-Host "S3Upload: Running AWS command with debug mode enabled."
     
     Invoke-Expression $awsCommand
     if ($LastExitCode -ne 0) {
         throw "AWS S3 copy failed with exit code $LastExitCode."
     }
-    Write-VerboseLog "AWS S3 copy completed successfully."
+    Write-Host "S3Upload: AWS S3 copy completed successfully."
 }
 catch {
-    Write-VerboseLog "Error: $_"
+    Write-Host "S3Upload: Error: $_"
     throw
 }
