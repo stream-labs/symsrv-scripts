8cb0bae4711573d0e0b9969bae849799a16eefb8
Vladimir <summeroff@gmail.com>
2023-11-14 12:16:41 -0800

    add logging to s3upload.ps1

--- s3upload.ps1
+++ s3upload.ps1
@@ -1,16 +1,36 @@
 param(
-       # The directory path of the github project
+       # The directory path of the GitHub project
        [Parameter(Mandatory = $true)]
        [string] $symStoreFolder
 )
 
-# Local environment variables, even if there are system ones with the same name, these are used for the cmd below
+# Function for verbose logging
+function Write-VerboseLog {
+    param (
+        [string] $message
+    )
+    if ($Env:VERBOSE_SYMB_UPLOAD -eq "true") {
+        Write-Host "VERBOSE: $message"
+    }
+}
+
+# Setting local environment variables
+Write-VerboseLog "Setting AWS environment variables..."
 $Env:AWS_ACCESS_KEY_ID = $Env:AWS_SYMB_ACCESS_KEY_ID
 $Env:AWS_SECRET_ACCESS_KEY = $Env:AWS_SYMB_SECRET_ACCESS_KEY
 $Env:AWS_DEFAULT_REGION = "us-east-2"
+Write-VerboseLog "AWS environment variables set."
 
-aws s3 cp $symStoreFolder s3://slobs-symbol.streamlabs.com/symbols --recursive --acl public-read
-
-if ($LastExitCode) {
-	throw
+# AWS S3 Copy
+Write-VerboseLog "Starting AWS S3 copy..."
+try {
+    aws s3 cp $symStoreFolder s3://slobs-symbol.streamlabs.com/symbols --recursive --acl public-read
+    if ($LastExitCode -ne 0) {
+        throw "AWS S3 copy failed with exit code $LastExitCode."
+    }
+    Write-VerboseLog "AWS S3 copy completed successfully."
+}
+catch {
+    Write-VerboseLog "Error: $_"
+    throw
 }
